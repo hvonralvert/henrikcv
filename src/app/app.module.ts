84bae86d217f1defadfc4da0c258da50ff60699c
henrik <henrik@ralva.se>
2019-02-18 11:10:03 +0100

    Basics added. Firebase, Angluar Material, TSConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC8TPoL67PkiLYNFjCTwMoF2y3RqDlMr5M',
+    authDomain: 'henrikscv-d2fc3.firebaseapp.com',
+    databaseURL: 'https://henrikscv-d2fc3.firebaseio.com',
+    projectId: 'henrikscv-d2fc3',
+    storageBucket: 'henrikscv-d2fc3.appspot.com',
+    messagingSenderId: '900545843507'
+  }
 };
 
 /*
